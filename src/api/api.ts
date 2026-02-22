c3348a5e4bccd88904869fd502427c1c7fe4554a
vikas8669 <vikayadav8669@gmail.com>
2026-02-22 12:59:54 +0530

    revmove vercel

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios  from 'axios'
 
 
-const baseUrl = "https://vk-backend-psi.vercel.app/api/v1"
+const baseUrl = import.meta.env.VITE_API_URL
 
 // const baseUrl = "http://localhost:8000/api/v1/"
 
